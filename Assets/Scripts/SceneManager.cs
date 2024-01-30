1ad4c37746454dcea3555ac1784b1f9eabf02cb5
yunchaowei <yunchao.wei01@estudiant.upf.edu>
2024-01-30 19:02:34 +0100

    scene manager created

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject Furnitures;
+    public bool CanChangeHeight = false;
+    public float height = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
