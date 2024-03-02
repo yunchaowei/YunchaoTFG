2c70ae73b0b097aff78e9518387a3200d6da9075
Yunchao_Wei <yunchao.wei01@estudiant.upf.edu>
2024-03-02 11:43:48 +0100

    wall & floor updated

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -31,6 +31,7 @@ public class SceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, UserHeight, Camera.main.transform.position.z);
         switch (HeightOption)
         {
             case HeightOptions.Low:
