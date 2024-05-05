b526e06438d8315f951af5157eb6c8a25c39d689
Yunchao_Wei <yunchao.wei01@estudiant.upf.edu>
2024-05-05 13:31:02 +0200

    Canvas UI created

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using static SceneManager;
+using static System.Net.Mime.MediaTypeNames;
+using static UnityEngine.Rendering.DebugUI;
 
 public class SceneManager : MonoBehaviour
 {
@@ -20,9 +25,13 @@ public class SceneManager : MonoBehaviour
     private int _heightOptions = 0;
     public HeightOptions HeightOption = HeightOptions.Medium;
     public string UserName = "Test User";
-    [Range(0.1f, 2.00f)]public float UserHeight;
+    const float MinUserHeight = 1.4f;
+    const float MaxUserHeight = 2f;
+    [Range(MinUserHeight, MaxUserHeight)]public float UserHeight;
     [Range(1.00f, 5.00f)] public float MaxDistanceCasting;
     public GameObject Furnitures;
+    private float FurnituresOriginalHeight = 0;
+    private float OVRCameraRigOriginalHeight = 0;
     public bool CanChangeHeight = false;
     //[Range(0.01f, 0.50f)] public float height = 0;
     private float height = 0;
@@ -36,6 +45,12 @@ public class SceneManager : MonoBehaviour
     public GameObject cameraParent;
     public GameObject OVRCameraRig;
     public float finalHeight;
+    public Scrollbar scrollbarUserHeight;  
+    public TextMeshProUGUI texUsertHeight;
+    public UnityEngine.UI.Button btnUserConfigSave;
+    public TMP_InputField InputFieldUserID;
+    public ToggleGroup toggleGroup;
+    public GameObject CanvasUserConfig;
     //public GameObject cameraY;
     // Start is called before the first frame update
     void Start()
@@ -46,7 +61,8 @@ public class SceneManager : MonoBehaviour
                                                         UserHeight - 0.1f,
                                                         OVRCameraRig.transform.position.z);
         }
-
+        FurnituresOriginalHeight = Furnitures.transform.position.y;
+        OVRCameraRigOriginalHeight = OVRCameraRig.transform.position.y;
         //Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, UserHeight, Camera.main.transform.position.z);
         switch (HeightOption)
        {
@@ -66,10 +82,55 @@ public class SceneManager : MonoBehaviour
                 _heightOptions = 4;
                 break;
         }
-        _changeHeights(true);
+        //_changeHeights(true);
+        _setHeights();
 
-
+        scrollbarUserHeight.onValueChanged.AddListener(UpdateTextUsertHeight);
+        btnUserConfigSave.onClick.AddListener(btnUserConfigSaveClicked);
+        UpdateTextUsertHeight(scrollbarUserHeight.value);
         StartCoroutine(CollectDataRoutine());
+    }
+
+    private void btnUserConfigSaveClicked()
+    {
+        UserName = InputFieldUserID.text;
+        UserHeight = float.Parse(texUsertHeight.text);
+        UnityEngine.UI.Toggle selectedToggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        if(selectedToggle != null)
+        {
+            switch (selectedToggle.name)
+            {
+                case "Toggle_None":
+                    HeightOption = HeightOptions.None;
+                    break;
+                case "Toggle_Low":
+                    HeightOption = HeightOptions.Low;
+                    break;
+                case "Toggle_Medium":
+                    HeightOption = HeightOptions.Medium;
+                    break;
+                case "Toggle_High":
+                    HeightOption = HeightOptions.High;
+                    break;
+                case "Toggle_VeryHigh":
+                    HeightOption = HeightOptions.VeryHigh;
+                    break;
+            }
+            _setHeights();
+        }
+
+        CanvasUserConfig.SetActive(false);
+    }
+
+    private void UpdateTextUsertHeight(float arg0)
+    {
+        float currentHeight = Mathf.Lerp(MinUserHeight, MaxUserHeight, scrollbarUserHeight.value);
+        texUsertHeight.text = currentHeight.ToString("0.00")/* + " m"*/;
+    }
+
+    void OnDestroy()
+    {
+        scrollbarUserHeight.onValueChanged.RemoveListener(UpdateTextUsertHeight);
     }
 
     private void Update()
@@ -94,6 +155,71 @@ public class SceneManager : MonoBehaviour
                 _heightOptions = 0;
             else
                 _changeHeights(false);
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("B button was pressed on Right Touch Controller.");
+            OpsUserConfigMenu(false);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("A button was pressed on Right Touch Controller.");
+            OpsUserConfigMenu(true);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("B button was pressed on Right Touch Controller.");
+            OpsUserConfigMenu(false);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("A button was pressed on Right Touch Controller.");
+            OpsUserConfigMenu(true);
+        }
+    }
+
+    private void OpsUserConfigMenu(bool open)
+    {
+        CanvasUserConfig.SetActive(open);
+    }
+
+    private void _setHeights()
+    {
+        float additionalHeight = 0.0f;
+        switch (HeightOption)
+        {
+            case HeightOptions.None:
+                break;
+            case HeightOptions.Low:
+                additionalHeight = 0.15f;
+                break;
+            case HeightOptions.Medium:
+                additionalHeight = 0.3f;
+                break;
+            case HeightOptions.High:
+                additionalHeight = 0.45f;
+                break;
+            case HeightOptions.VeryHigh:
+                additionalHeight = 0.6f;
+                break;
+        }
+        finalHeight = UserHeight + additionalHeight;
+        if (CanChangeHeight)
+        {
+            Furnitures.transform.position = new Vector3(Furnitures.transform.position.x,
+                                                        FurnituresOriginalHeight + additionalHeight,
+                                                        Furnitures.transform.position.z);
+            //foreach (GameObject go in FurnituresScaled)
+            //{
+            //    go.transform.localScale = new Vector3(go.transform.localScale.x,
+            //                                          go.transform.localScale.y+height,  //(go.transform.parent.transform.position.y + height)/ go.transform.parent.transform.position.y,
+            //                                            go.transform.localScale.z);
+            //}
+            if (OVRCameraRig != null)
+            {
+                finalHeight = OVRCameraRigOriginalHeight + height;
+            }
         }
     }
 
@@ -223,7 +349,7 @@ public class SceneManager : MonoBehaviour
         dataList.userName = UserName;
         dataList.userHeight = UserHeight;
         string json = JsonUtility.ToJson(dataList, true);
-        File.WriteAllText(Application.dataPath + $"/DataFolder/{UserName}.json", json);
-        Debug.Log("Data saved in path: " + Application.dataPath + $"/DataFolder/{UserName}.json");
+        File.WriteAllText(UnityEngine.Application.dataPath + $"/DataFolder/{UserName}.json", json);
+        Debug.Log("Data saved in path: " + UnityEngine.Application.dataPath + $"/DataFolder/{UserName}.json");
     }
 }
